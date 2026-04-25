b5adf1c02496684330222bfa8cf530ea22c42d32
souravxdev <sourav.seu.cse@gmail.com>
2026-04-25 22:47:39 +0600

    set up shadcn ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState('');
+    const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(response => response.json())
-      .then(data => setMessage(data.message));
-  }, []);
+    useEffect(() => {
+        fetch('/api/hello')
+            .then((response) => response.json())
+            .then((data) => setMessage(data.message));
+    }, []);
 
-  return <p className='font-bold p-4 text-3xl'>{message}</p>
+    return (
+        <div className="p-4">
+            <p className="font-bold text-3xl">{message}</p>
+            <Button>Click Me!</Button>
+        </div>
+    );
 }
 
-export default App
+export default App;
