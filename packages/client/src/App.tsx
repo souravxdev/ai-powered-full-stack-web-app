de222bb459f2a83eefa6969d8fab6a3dcf9c3cbf
souravxdev <sourav.seu.cse@gmail.com>
2026-04-25 23:17:43 +0600

    set up husky with bun

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
    return (
       <div className="p-4">
          <p className="font-bold text-3xl">{message}</p>
-         <Button>Click Me!</Button>
+         <Button>Click Me</Button>
       </div>
    );
 }
